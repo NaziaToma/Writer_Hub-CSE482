1fdae6ff788096e4a3bcbdd54fdf3c9f6dcf0c93
NaziaToma <nazia.tabassum@northsouth.edu>
2020-12-23 18:47:37 +0600

    cookie added to the website

---
+++ createcookie.php
@@ -0,0 +1,10 @@
+<?php
+
+$con= mysqli_connect('localhost','root','','cookie');
+if(isset($_COOKIE['visit'])){
+
+}else{
+    setCookie('visit', 'yes', time()+(60*60*24*30));
+    mysqli_query($con, "update visit set total_count = total_count+1");
+}
+?>

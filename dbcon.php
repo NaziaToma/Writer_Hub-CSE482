b3311465674c44651bda22bd0fef104177c4ad4f
NaziaToma <nazia.tabassum@northsouth.edu>
2020-12-30 19:10:20 +0600

    updated hosting in dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,3 +1,15 @@
+
+<?php
+/*
+$server = "sql103.epizy.com";
+$user = "epiz_27576584";
+$password = "3GIs5MxPmt";
+$db = "epiz_27576584_writerhub";
+
+$con = mysqli_connect( $server,$user,$password,$db);
+*/
+?>
+
 <?php
 $server = "localhost";
 $user = "root";
